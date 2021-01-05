be5bf439ac935e7a16e4442a180b867637b6631f
Theo Vanderkooy <theo.vanderkooy@gmail.com>
2021-01-05 13:30:24 -0500

    basic functionality working

--- README.md
+++ README.md
@@ -8,8 +8,10 @@ An extension for google chrome to open the current page with the UW library prox
 - make it more general, usable with other proxies?
 - documentation: installation, etc...
 - icon
+- close original tab?
 - finish it...
 - manifest version 3 w/ chrome 88
+- list of domains to automatically redirect?
 
 ## Bugs/issues
 - ...
--- test.js
+++ test.js
@@ -1,18 +1,62 @@
-function setTabURL() {
+// function setTabURL() {
+//     chrome.tabs.query( {
+//             active: true,
+//             currentWindow: true,
+//             // lastFocusedWindow: true,
+//         },
+//         tabs => {
+//             document.getElementById("testoutput").textContent = tabs[0].url;
+//         });
+// }
+// document.addEventListener("DOMContentLoaded", setTabURL);
 
-    chrome.tabs.query( {
-            active: true,
-            currentWindow: true,
-            // lastFocusedWindow: true,
-        },
-        tabs => {
-            document.getElementById("testoutput").textContent = tabs[0].url;
-            // alert(tabs[0]?.url);
-            // TODO -> parse out the URL and replace as appropriate
-            // TODO on icon click event...
-        });
+const proxy_host = ".proxy.lib.uwaterloo.ca";
 
+
+function translateURL(url) {
+    url = url.split("://")[1]; // remove protocol from URL
+    let url_parts = url.split("/"); // split by /
+    let hostname = url_parts.shift(); // hostname needs to be changed
+
+    // check if it is already a proxy link, null indicates nothing to translate
+    if (hostname.includes(proxy_host)) {
+        return null;
+    }
+
+    // construct and return new URL
+    let res = "https://" + hostname.replaceAll(".","-") + proxy_host + "/" + url_parts.join("/");
+
+    return res;
 }
 
 
-document.addEventListener("DOMContentLoaded", setTabURL);
+chrome.browserAction.onClicked.addListener(tab => {
+    // chrome.tabs.query( {
+    //         active: true,
+    //         currentWindow: true,
+    //     },
+    //     tabs => {
+    //         var url = tabs[0].url;
+    //         // alert(translateURL(url));
+    //     });
+
+    // new URL
+    let proxyURL = translateURL(tab.url);
+
+    // alert(proxyURL);
+
+    // early return if we're already in the proxy
+    if (!proxyURL) return;
+    // TODO!!! maybe show an error message?
+
+    chrome.tabs.create({
+        active: true,
+        selected: true,
+        openerTabId: tab.id,
+        index: tab.index + 1,
+        url: proxyURL,
+    })
+});
+
+
+// .proxy.lib.uwaterloo.ca
